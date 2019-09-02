f3d79055b7f948bfb0312c95de6e36365a75b1db
UID <UID@DESKTOP-DACJOCN>
2019-09-02 10:29:08 +0800

    springcloud

---
+++ sp02-itemservice/src/main/java/com/tedu/sp02/item/service/ItemServiceImpl.java
@@ -0,0 +1,38 @@
+package com.tedu.sp02.item.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tedu.sp01.pojo.Item;
+import com.tedu.sp01.service.ItemService;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@Service
+public class ItemServiceImpl implements ItemService{
+	
+	@Override
+	public List<Item> getItems(String orderId) {
+		ArrayList<Item> list = new ArrayList<Item>();
+		list.add(new Item(1, "aaa", 6));
+		list.add(new Item(2, "bbb", 5));
+		list.add(new Item(3, "ccc", 4));
+		list.add(new Item(4, "ddd", 3));
+		list.add(new Item(5, "eee", 2));
+		list.add(new Item(6, "fff", 1));
+		return list;
+	}
+
+	@Override
+	public void decreaseNumbers(List<Item> list) {
+		if (log.isInfoEnabled()) {
+			for (Item item : list) {
+				log.info("减少库存-"+item);
+			}
+		}
+		
+	}
+
+}
